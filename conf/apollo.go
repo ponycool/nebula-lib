c7d1d105dbc7c78dec3a21e0aadbf8289b543c80
Pony <pony@ponycool.com>
2022-09-20 21:22:22 +0800

    Update apollo.go

--- conf/apollo.go
+++ conf/apollo.go
@@ -1,7 +1,9 @@
 package conf
 
 import (
+	"fmt"
 	"github.com/apolloconfig/agollo/v4"
+	"github.com/apolloconfig/agollo/v4/agcache"
 	"github.com/apolloconfig/agollo/v4/env/config"
 	"sync"
 )
@@ -12,8 +14,8 @@ var (
 	conf    agollo.Client
 )
 
-// Conf 配置结构体
-type Conf struct {
+// Options  配置结构体
+type Options struct {
 	AppID          string `json:"app_id"`
 	Cluster        string `json:"cluster"`
 	IP             string `json:"ip"`
@@ -23,7 +25,7 @@ type Conf struct {
 }
 
 // Init 初始化
-func Init(c *Conf) (conf agollo.Client, err error) {
+func Init(opts *Options) (conf agollo.Client, err error) {
 	rwLock.Lock()
 	defer rwLock.Unlock()
 
@@ -31,16 +33,17 @@ func Init(c *Conf) (conf agollo.Client, err error) {
 		return conf, nil
 	}
 
-	Conf := &config.AppConfig{
-		AppID:          c.AppID,
-		Cluster:        c.Cluster,
-		IP:             c.IP,
-		NamespaceName:  c.NamespaceName,
-		IsBackupConfig: c.IsBackupConfig,
-		Secret:         c.Secret,
+	c := &config.AppConfig{
+		AppID:          opts.AppID,
+		Cluster:        opts.Cluster,
+		IP:             opts.IP,
+		NamespaceName:  opts.NamespaceName,
+		IsBackupConfig: opts.IsBackupConfig,
+		Secret:         opts.Secret,
 	}
+
 	conf, err = agollo.StartWithConfig(func() (*config.AppConfig, error) {
-		return Conf, nil
+		return c, nil
 	})
 	if err != nil {
 		return nil, err
@@ -50,7 +53,22 @@ func Init(c *Conf) (conf agollo.Client, err error) {
 	return conf, nil
 }
 
-// Get 获取配置
-func Get() agollo.Client {
-	return conf
+// Cache 配置缓存
+func Cache(namespace string, client agollo.Client) agcache.CacheInterface {
+	cache := client.GetConfigCache(namespace)
+	return cache
+}
+
+// CheckKey 检查配置键
+func CheckKey(namespace string, client agollo.Client) {
+	cache := client.GetConfigCache(namespace)
+	count := 0
+	cache.Range(func(key, value interface{}) bool {
+		fmt.Println("key : ", key, ", value :", value)
+		count++
+		return true
+	})
+	if count < 1 {
+		panic("config key can not be null")
+	}
 }
