6011383081a4c9dd889474347d811966e3542dcb
Pony <pony@ponycool.com>
2022-11-15 14:59:17 +0800

    Update datetime_test.go

--- test/datetime_test.go
+++ test/datetime_test.go
@@ -13,3 +13,23 @@ func TestTimeStringToTime(t *testing.T) {
 	time := datetime.Time{}.TimeStringToTime(timeStr)
 	fmt.Println(time)
 }
+
+func TestGetNowSec(t *testing.T) {
+	timestamp := datetime.GetNowSec()
+	fmt.Println(fmt.Sprintf("当前Unix时间戳为：%d，精确到秒", timestamp))
+}
+
+func TestGetNowMs(t *testing.T) {
+	timestamp := datetime.GetNowMs()
+	fmt.Println(fmt.Sprintf("当前Unix时间戳为：%d，精确到毫秒", timestamp))
+}
+
+func TestGetNowUs(t *testing.T) {
+	timestamp := datetime.GetNowUs()
+	fmt.Println(fmt.Sprintf("当前Unix时间戳为：%d，精确到微妙", timestamp))
+}
+
+func TestGetNowNs(t *testing.T) {
+	timestamp := datetime.GetNowNs()
+	fmt.Println(fmt.Sprintf("当前Unix时间戳为：%d，精确到纳秒", timestamp))
+}
