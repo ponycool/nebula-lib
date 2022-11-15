fb77cdd41d22231a261940edbfa89627c5a8a9c1
Pony <pony@ponycool.com>
2022-11-15 11:27:46 +0800

    Create db.go

---
+++ constants/db.go
@@ -0,0 +1,21 @@
+package constants
+
+// DataDeleted 逻辑删除状态：已删除
+func DataDeleted() []uint8 {
+	return []uint8{1}
+}
+
+// DataUNDELETED 逻辑删除状态：正常
+func DataUNDELETED() []uint8 {
+	return []uint8{0}
+}
+
+// DataLocked 冻结状态：已冻结
+func DataLocked() []uint8 {
+	return []uint8{1}
+}
+
+// DataUnlocked 冻结状态：正常
+func DataUnlocked() []uint8 {
+	return []uint8{0}
+}
